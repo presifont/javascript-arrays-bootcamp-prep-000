b87105c5d4216063593fe4ce0f0bcc4a48b9dbb6
Daniel McMahon <daniel.m.mve@gmail.com>
2017-12-12 22:19:12 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+//Creates Array with candy types
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(chocolateBars, twix) {
+return [twix,...chocolateBars];
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, twix) {
+  chocolateBars.unshift(twix);
+  return chocolateBars;
+}
+function addElementToEndOfArray (chocolateBars, ruth) {
+  return [...chocolateBars, ruth];
+}
+function destructivelyAddElementToEndOfArray (chocolateBars, ruth) {
+  chocolateBars.push(ruth);
+  return chocolateBars;
+}
+function accessElementInArray(array, index){
+  return array[index];
+}
