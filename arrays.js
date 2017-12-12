0711e014257cc00f07ccc98536190e546d81b64f
Daniel McMahon <daniel.m.mve@gmail.com>
2017-12-12 22:38:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
-function destructivelyRemoveElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
